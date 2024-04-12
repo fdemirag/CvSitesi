562cf883ccedf27d0f2c46b1dac813af9e331e00
fdemirag <f2000fatih@hotmail.com>
2024-04-12 05:28:23 +0300

    migration yapildi

---
+++ CvSitesi/DataAccess/Context/MyPortfolio.cs
@@ -0,0 +1,23 @@
+﻿using CvSitesi.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CvSitesi.DataAccess.Context
+{
+    public class MyPortfolio:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; initial catalog=MyPortfolio; integrated security=true");
+        }
+
+        public DbSet<About>Abouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+    }
+}
