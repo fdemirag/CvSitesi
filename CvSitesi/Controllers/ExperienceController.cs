ba18057ebe68c4cdad67fd2b01005df3745d478a
fdemirag <f2000fatih@hotmail.com>
2024-05-04 00:50:23 +0300

    Adminpanel üzerinden ekleme yapıldı

--- CvSitesi/Controllers/ExperienceController.cs
+++ CvSitesi/Controllers/ExperienceController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CvSitesi.DataAccess.Context;
+using CvSitesi.DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvSitesi.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
+
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
-    }
+		[HttpGet]
+		public IActionResult CreateExperience()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateExperience(Experience experience)
+		{
+			context.Experiences.Add(experience);
+			context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+	}
 }
